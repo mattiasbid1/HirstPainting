8bc5a4f66c07b242740828a226665d52a2b7a880
Mattias Björk <mattias.bjork.id1@gmail.com>
2023-05-17 21:25:53 +0200

    final

--- main.py
+++ main.py
@@ -1,2 +1,29 @@
-# start
+from turtle import Turtle, Screen
+import random
 
+
+def color():
+    return (random.random(), random.random(), random.random())
+
+def dot_painter(x, y):
+    timmy.penup()
+    timmy.setposition(-180, -180)
+    for i in range(x):
+
+        for j in range(y):
+            timmy.dot(20, color())
+            timmy.forward(50)
+
+        timmy.setposition(-180, (-180 + 50 * (i+1)))
+
+
+
+timmy = Turtle()
+screen = Screen()
+
+timmy.speed(0)
+timmy.hideturtle()
+
+dot_painter(10, 10)
+
+screen.exitonclick()
